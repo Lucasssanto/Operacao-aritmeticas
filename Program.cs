24bf35b06a9d7c3f980fde1d24af84ecbd37b48e
Lucas Dos Santos <66095197+Lucasssanto@users.noreply.github.com>
2021-12-11 10:22:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroQualquer = 20 + 20;
+            int numeroMult = 10 * 100 ;
+            float numeroDiv = 5.0f / 2;
+
+            int test = 500 / 15 - 150 - + (285 * 386);
+
+            // 6 / 2=> // int
+            // 5 / 2=> 2.5 // decimal // float
+
+            Console.WriteLine(numeroQualquer);
+            Console.WriteLine(numeroMult);
+            Console.WriteLine(numeroDiv);
+            Console.ReadLine();
+           
+        }
+    }
+}
